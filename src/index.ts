31dce82c8cfa4f70807e8c45d5145734416a9e6f
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-03-01 08:38:01 +0900

    20250301 First Commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// src/index.ts
+import express from 'express';
+import { createHandler } from 'graphql-http/lib/use/express';
+import { schema } from './schema';
+import { resolvers } from './resolver';
+
+
+const app = express();
+app.all('/graphql', createHandler({
+  schema: schema,
+  rootValue: resolvers,
+}));
+
+app.listen(4000, () => console.log('Server running on http://localhost:4000/graphql'));
