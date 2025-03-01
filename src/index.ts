ce7013f12f897401543ae76ac7d162c949d695fd
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-03-01 19:35:26 +0900

    20250301 feat: cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
 import { schema } from './schema';
 import { resolvers } from './resolver';
-
+import cors from 'cors';
 
 const app = express();
+
+// CORSを有効にする
+app.use(cors());
+
 app.all('/graphql', createHandler({
   schema: schema,
   rootValue: resolvers,
 }));
+
 
 app.listen(4000, () => console.log('Server running on http://localhost:4000/graphql'));
